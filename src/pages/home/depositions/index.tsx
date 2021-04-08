d2a4c8894cb0ee496f44ea7cbd0944cf295b9ef7
Dionei Bianchati <dion.biancha@gmail.com>
2021-04-08 08:30:05 -0300

    feature: Depositions slider

--- src/pages/home/depositions/index.tsx
+++ src/pages/home/depositions/index.tsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const items = [
+  {
+    text: 'Teste 1',
+    ranking: 5.0
+  },
+  {
+    text: 'Teste 2',
+    ranking: 3.0
+  },
+  {
+    text: 'Teste 3',
+    ranking: 4.0
+  }
+]
 
 const Depositions: React.FC = () => {
+  const [deposition, setDeposition] = useState(0)
   return (
     <div className="flex flex-col">
-      <div className="flex flex-row items-center mt-32">
+      <div className="flex sm:flex-row flex-col items-center mt-32">
         <h2 className="text-white text-2xl">Depoimentos</h2>
-        <h3 className="text-gray_2 italic text-sm ml-10">
+        <h3 className="text-gray_2 italic text-sm sm:ml-10">
           O que os clientes falam sobre mim
         </h3>
       </div>
       <section>
         <div className="container px-5 py-16 mx-auto">
           <div className="flex flex-wrap -m-4">
-            <div className="w-full h-full bg-gray_3 p-8 rounded">
+            <div className="w-full h-full bg-gray_3 sm:p-12 p-5 rounded">
               <div className="flex flex-row">
                 <div className="flex mb-4">
                   <span className="flex items-center">
                     <svg
-                      fill="currentColor"
+                      fill={
+                        items[deposition].ranking >= 1.0
+                          ? 'currentColor'
+                          : 'none'
+                      }
                       stroke="currentColor"
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -28,7 +48,11 @@ const Depositions: React.FC = () => {
                       <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                     </svg>
                     <svg
-                      fill="currentColor"
+                      fill={
+                        items[deposition].ranking >= 2.0
+                          ? 'currentColor'
+                          : 'none'
+                      }
                       stroke="currentColor"
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -39,7 +63,11 @@ const Depositions: React.FC = () => {
                       <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                     </svg>
                     <svg
-                      fill="currentColor"
+                      fill={
+                        items[deposition].ranking >= 3.0
+                          ? 'currentColor'
+                          : 'none'
+                      }
                       stroke="currentColor"
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -50,7 +78,11 @@ const Depositions: React.FC = () => {
                       <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                     </svg>
                     <svg
-                      fill="currentColor"
+                      fill={
+                        items[deposition].ranking >= 4.0
+                          ? 'currentColor'
+                          : 'none'
+                      }
                       stroke="currentColor"
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -61,7 +93,11 @@ const Depositions: React.FC = () => {
                       <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                     </svg>
                     <svg
-                      fill="none"
+                      fill={
+                        items[deposition].ranking === 5.0
+                          ? 'currentColor'
+                          : 'none'
+                      }
                       stroke="currentColor"
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -72,22 +108,19 @@ const Depositions: React.FC = () => {
                       <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                     </svg>
                     <span className="text-gray_2 text-sm italic ml-3">
-                      Avaliação 5.0
+                      Avaliação {items[deposition].ranking}
                     </span>
                   </span>
                 </div>
               </div>
               <div className="flex flex-row justify-center">
-                <p className="leading-relaxed p-5 w w-4/5 text-white text-lg ">
-                  Synth chartreuse iPhone lomo cray raw denim brunch everyday
-                  carry neutra before they sold out fixie 90s microdosing. Tacos
-                  pinterest fanny pack venmo, post-ironic heirloom try-hard
-                  pabst authentic iceland.
+                <p className="leading-relaxed p-5 w w-4/5 text-white text-sm ">
+                  {items[deposition].text}
                 </p>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
-                  className="block w-16 h-16 text-gray_4 mb-4"
+                  className="block sm:w-16 sm:h-16 w-8 h-8 text-gray_4 mb-4"
                   viewBox="0 0 975.036 975.036"
                 >
                   <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
@@ -98,13 +131,16 @@ const Depositions: React.FC = () => {
         </div>
       </section>
       <div className="flex flex-row w-full justify-between">
-        <div className="flex flex-row items-center mb-5">
+        <button
+          className="flex flex-row items-center mb-5 cursor-pointer focus:outline-none"
+          onClick={() => setDeposition(0)}
+        >
           <img
-            className="w-16 h-16 rounded-full"
+            className="w-16 h-16 rounded-full sm:block hidden"
             src="assets/images/me.jpg"
             alt="Foto de Dionei Bianchati"
           />
-          <div className="flex flex-col ml-5">
+          <div className="flex flex-col sm:ml-5">
             <h4 className="italic text-white">Lucas Luvato</h4>
             <p className="text-sm text-gray_2 italic">
               Co-founder,{' '}
@@ -113,14 +149,17 @@ const Depositions: React.FC = () => {
               </span>
             </p>
           </div>
-        </div>
-        <div className="flex flex-row items-center mb-5">
+        </button>
+        <button
+          className="flex flex-row items-center mb-5 cursor-pointer focus:outline-none"
+          onClick={() => setDeposition(1)}
+        >
           <img
-            className="w-16 h-16 rounded-full"
+            className="w-16 h-16 rounded-full sm:block hidden"
             src="assets/images/me.jpg"
             alt="Foto de Dionei Bianchati"
           />
-          <div className="flex flex-col ml-5">
+          <div className="flex flex-col sm:mr-0 sm:ml-5 ml-3 mr-3">
             <h4 className="italic text-white">Lucas Luvato</h4>
             <p className="text-sm text-gray_2 italic">
               Co-founder,{' '}
@@ -129,14 +168,17 @@ const Depositions: React.FC = () => {
               </span>
             </p>
           </div>
-        </div>
-        <div className="flex flex-row items-center mb-5">
+        </button>
+        <button
+          className="flex flex-row items-center mb-5 cursor-pointer focus:outline-none"
+          onClick={() => setDeposition(2)}
+        >
           <img
-            className="w-16 h-16 rounded-full"
+            className="w-16 h-16 rounded-full sm:block hidden"
             src="assets/images/me.jpg"
             alt="Foto de Dionei Bianchati"
           />
-          <div className="flex flex-col ml-5">
+          <div className="flex flex-col sm:ml-5">
             <h4 className="italic text-white">Lucas Luvato</h4>
             <p className="text-sm text-gray_2 italic">
               Co-founder,{' '}
@@ -145,10 +187,17 @@ const Depositions: React.FC = () => {
               </span>
             </p>
           </div>
-        </div>
+        </button>
       </div>
       <div className="w-full h-1 rounded bg-gray_3 mt-2">
-        <div className="w-1/3 h-1 rounded bg-green_1" />
+        <div
+          style={{
+            transition: 'all 0.2s linear',
+            marginLeft:
+              deposition === 1 ? '33.333%' : deposition === 2 ? '66.666%' : '0%'
+          }}
+          className="w-1/3 h-1 rounded bg-green_1"
+        />
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {},
     colors: {
       green_1: '#32FF84',
+      green_2: 'rgb(50,255,132, 0.1)',
       white: '#FFFFFF',
       yellow: '#E1C116',
       gray_1: '#E5E5E5',
