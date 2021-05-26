2cf51452cc8bfdeeaae175010f67133fd4f74045
Dionei Bianchati <dionbiancha@gmail.com>
2021-05-25 22:45:53 -0300

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
+import '../styles/tailwind.css'
 import '../styles/style.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
